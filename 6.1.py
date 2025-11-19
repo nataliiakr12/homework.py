1a2c452276a04b7a20dc35ffa73b3d1b8292d950
VICTUS <natka08121996@gmail.com>
2025-11-19 23:13:23 +0100

    6.1. Діапазон букв

---
+++ 6.1.py
@@ -0,0 +1,6 @@
+import string
+letters = string.ascii_letters
+print(letters [letters.index('a'):letters.index('c')+1])
+print(letters [letters.index('a'):letters.index('a')+1])
+print(letters [letters.index('s'):letters.index('H')+1])
+print(letters [letters.index('a'):letters.index('A')+1])
